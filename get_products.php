57128ab4cc4e82c97fc8e7b2cb6f511656f10960
TIINA PELIMANNI <tiina.pelimanni@gmail.com>
2022-03-29 10:31:44 +0300

    tuotteiden haku tuoteryhmän mukaan

--- get_products.php
+++ get_products.php
@@ -2,23 +2,25 @@
 require_once "inc/functions.php";
 require_once 'inc/headers.php';
 
-$id = $_GET["id"];
-
-if( !isset($id) ){
-    echo "Parametreja puuttui";
-    exit;
-}
-
-$SQL = "SELECT * FROM tuote";
-
-if($id > 0){
-    $SQL = "SELECT * FROM tuote WHERE trnro = " . $id;
-}
+$uri = parse_url(filter_input(INPUT_SERVER, 'PATH_INFO'), PHP_URL_PATH);
+$parameters = explode("/", $uri);
+$category_id = $parameters[1];
 
 try {
     $db = openDB();
-    selectAsJson($db, $SQL);
-}
-catch (PDOException $pdoex) {
+    $sql = "SELECT * FROM tuoteryhma WHERE trnro = $category_id";
+    $query = $db->query($sql);
+    $category = $query->fetch(PDO::FETCH_ASSOC);
+
+    $sql = "SELECT * FROM tuote WHERE trnro = $category_id";
+    $query = $db->query($sql);
+    $products = $query->fetchAll(PDO::FETCH_ASSOC);
+
+    header("HTTP/1.1 200 OK");
+    echo json_encode(array(
+        "category" => $category["trnimi"],
+        "products" => $products
+    ),JSON_PRETTY_PRINT);
+} catch (PDOException $pdoex) {
     returnError($pdoex);
 }

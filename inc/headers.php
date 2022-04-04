3e0350c379d22b2a7b61947965b57537c9bb13af
n1vial00 <n1vial00@students.oamk.fi>
2022-04-04 15:20:47 +0300

    Headerit muutettu CORS errorien takia pois localhost muodosta.

--- inc/headers.php
+++ inc/headers.php
@@ -1,17 +1,11 @@
 <?php
-header("Access-Control-Allow-Origin: http://localhost:3000");
+header("Access-Control-Allow-Origin:" . $_SERVER['HTTP_ORIGIN']);
 header("Access-Control-Allow-Credentials: true");
-header("Access-Control-Allow-Methods: POST, GET, PUT, DELETE, OPTIONS");
-header("Access-Control-Allow-Headers: Accept, Content-Type, Access-Control-Allow-Header");
-header("Content-Type: application/json");
+header("Access-Control-Allow-Method: GET, POST,PUT, DELETE, OPTIONS");
+header("Access-Control-Allow-Headers: Accept, Content-Type", "Access-Control-Allow-Header");
 header("Access-Control-Max-Age: 3600");
+header("Content-Type: application/json");
 
 if ($_SERVER["REQUEST_METHOD"] === "OPTIONS") {
-    if (isset($_SERVER["HTTP_ACCESS_CONTROL_REQUEST_METHOD"])) 
-        header("Access-Control-Allow-Methods: GET, POST, OPTIONS");        
-    
-    if (isset($_SERVER["HTTP_ACCESS_CONTROL_REQUEST_HEADERS"])) 
-        header("Access-Control-Allow-Headers: {$SERVER['HTTP_ACCESS_CONTROL_REQUEST_HEADERS']}");
-
-    exit(0);
+    return 0;
 }

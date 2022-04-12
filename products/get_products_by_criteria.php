042d7a34728d739c4c84d3e709022804e1bcc558
n1vial00 <n1vial00@students.oamk.fi>
2022-04-12 11:20:28 +0300

    Listätty nimellä haku

---
+++ products/get_products_by_criteria.php
@@ -0,0 +1,22 @@
+<?php
+require_once "../inc/functions.php";
+require_once '../inc/headers.php';
+
+$uri = parse_url(filter_input(INPUT_SERVER, 'PATH_INFO'), PHP_URL_PATH);
+$parameters = explode("/", $uri);
+$search_term = $parameters[1];
+
+try {
+    $db = openDB();
+
+    $sql = "SELECT * FROM tuote WHERE tuotenimi LIKE '%$search_term%'";
+    $query = $db->query($sql);
+    $products = $query->fetchAll(PDO::FETCH_ASSOC);
+
+    header("HTTP/1.1 200 OK");
+    echo json_encode(array(
+        "products" => $products
+    ),JSON_PRETTY_PRINT);
+} catch (PDOException $pdoex) {
+    returnError($pdoex);
+}

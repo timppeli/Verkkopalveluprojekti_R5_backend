8e90d8ed811ae1e0d08aaa0d5e4958ac0d521473
n1vial00 <n1vial00@students.oamk.fi>
2022-04-13 15:09:28 +0300

    Hups

--- inc/headers.php
+++ inc/headers.php
@@ -11,7 +11,7 @@ if ($_SERVER["REQUEST_METHOD"] === "OPTIONS") {
         header("Access-Control-Allow-Methods: GET, POST, OPTIONS");        
     
     if (isset($_SERVER["HTTP_ACCESS_CONTROL_REQUEST_HEADERS"])) 
-        header("Access-Control-Allow-Headers: {$SERVER['HTTP_ACCESS_CONTROL_REQUEST_HEADERS']}");
+        header("Access-Control-Allow-Headers: {$_SERVER['HTTP_ACCESS_CONTROL_REQUEST_HEADERS']}");
 
     exit(0);
 }

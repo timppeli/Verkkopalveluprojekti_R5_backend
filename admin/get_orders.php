acabd40656ff8170831ce20498f22b01aa1b4c0a
Tiina Pelimanni <tiina.pelimanni@gmail.com>
2022-04-20 20:50:47 +0300

    get_orders ei itke poistettujen tilausten perään

--- admin/get_orders.php
+++ admin/get_orders.php
@@ -4,54 +4,50 @@ require_once '../inc/headers.php';
 
 try {
   $db = openDB();
-  // Haetaan tilausten määrä
-  $sql = "SELECT COUNT(tilausnro) AS count FROM tilaus";
-  $query = $db->query($sql);
-  $numOfOrders = $query->fetch(PDO::FETCH_ASSOC)["count"];
 
-  $ordersArray = array();
+  // Haetaan tilausten tiedot
+  $sql = "SELECT tilausnro, CONCAT(SUBSTRING(tilausaika, 9, 2), '.', SUBSTRING(tilausaika, 6, 2), '.', YEAR(tilausaika), ' ', TIME(tilausaika)) as tilausaika, kasitelty, asiakas_id FROM tilaus";
+  $statement = $db->prepare($sql);
+  $statement->execute();
 
-  $i = 1;
-  while ($i <= $numOfOrders) {
-    // Haetaan tilaus
-    $sql = "SELECT tilausnro, tilausaika, kasitelty FROM tilaus WHERE tilausnro = $i";
-    $query = $db->query($sql);
-    $order = $query->fetch(PDO::FETCH_ASSOC);
+  if ($statement->rowCount() <= 0) {
+    throw new Exception("Ei tilauksia");
+  }
 
-    // Haetaan tilausrivien määrä
-    $sql = "SELECT COUNT(*) AS count FROM tilausrivi
-    LEFT JOIN tilaus ON tilausrivi.tilaus_id = tilaus.tilausnro";
-    $query = $db->query($sql);
-    $numOfRows = $query->fetch(PDO::FETCH_ASSOC)["count"];
+  $orders = $statement->fetchAll(PDO::FETCH_ASSOC);
+  $data = array();
 
-    $j = 0;
-    // Loopataan tilausrivien läpi
-    while ($j < $numOfRows) {
-      // Haetaan tilatut tuotteet
-      $sql = "SELECT tuotenro, tuotenimi, kpl FROM tuote
-    LEFT JOIN tilausrivi ON tilausrivi.tuote_id = tuote.tuotenro
-    LEFT JOIN tilaus ON tilaus.tilausnro = tilausrivi.tilaus_id WHERE tilausnro = $i";
-      $query = $db->query($sql);
-      $products = $query->fetchAll(PDO::FETCH_ASSOC);
-      $j++;
-    }
+  foreach ($orders as $order) {
+    // Asiakas
+    $sql = "SELECT etunimi, sukunimi, sposti, osoite, postinro, postitmp FROM asiakas WHERE asiakasnro = ?";
+    $statement = $db->prepare($sql);
+    $statement->bindParam(1, $order["asiakas_id"], PDO::PARAM_INT);
+    $statement->execute();
+    $customer = $statement->fetch(PDO::FETCH_ASSOC);
 
-    // Haetaan asiakastiedot
-    $sql = "SELECT asiakasnro, etunimi, sukunimi, osoite, postinro, postitmp FROM asiakas, tilaus WHERE asiakas.asiakasnro = tilaus.asiakas_id AND asiakasnro = $i";
-    $query = $db->query($sql);
-    $customer = $query->fetch(PDO::FETCH_ASSOC);
-    
-    // Lisätään tuotteet ja asiakastiedot tilaukseen
+    $order["asiakastiedot"] = $customer;
+
+    // Tuotteet
+    $sql = "SELECT tuotenro, tuotenimi, kpl FROM tuote JOIN tilausrivi ON tilausrivi.tuote_id = tuote.tuotenro WHERE tilaus_id = ?";
+    $statement = $db->prepare($sql);
+    $statement->bindParam(1, $order["tilausnro"], PDO::PARAM_INT);
+    $statement->execute();
+    $products = $statement->fetchAll(PDO::FETCH_ASSOC);
+
     $order["tuotteet"] = $products;
-    $order["tilaaja"] = $customer;
 
-    // Lisätään tilaus tilaustaulukkoon
-    array_push($ordersArray, $order);
-    $i++;
+    // Pusketaan kaikki tiedot lopulliseen tilaustaulukkoon
+    $data[] = $order;
+  }
+
+  // Tarkistetaan että tilauksia on
+  if ($data == []) {
+    throw new Exception("Ei tilauksia");
   }
 
   header("HTTP/1.1 200 OK");
-  echo json_encode($ordersArray, JSON_PRETTY_PRINT);
+  echo json_encode($data, JSON_PRETTY_PRINT);
 } catch (PDOException $pdoex) {
-  returnError($pdoex);
+  throw $pdoex;
+  echo $pdoex;
 }

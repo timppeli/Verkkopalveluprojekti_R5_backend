3d7648e97b94bad6ea5262904f685f915ee50d46
Aleksi Viitanen <n1vial00@students.oamk.fi>
2022-04-26 18:36:22 +0300

    Asiakkaan tunnuksen rekisteröinti toimii.

--- admin/add_user.php
+++ admin/add_user.php
@@ -5,8 +5,8 @@ require_once '../inc/headers.php';
 
 // Haetaan tiedot inputista
 $input = json_decode(file_get_contents("php://input"));
-$ktunnus = filter_var($input->etunimi, FILTER_SANITIZE_SPECIAL_CHARS);
-$salasana = filter_var($input->sukunimi, FILTER_SANITIZE_SPECIAL_CHARS);
+$ktunnus = filter_var($input->ktunnus, FILTER_SANITIZE_SPECIAL_CHARS);
+$salasana = filter_var($input->salasana, FILTER_SANITIZE_SPECIAL_CHARS);
 $sposti = filter_var($input->sposti, FILTER_SANITIZE_EMAIL);
 
 // Tarkistetaan, ettei yksikään kenttä ole tyhjä
@@ -21,8 +21,13 @@ try {
     $query->bindValue(":sposti", $sposti, PDO::PARAM_STR);
     $query->execute();
     
+    $asiakasnro = $query->fetch(PDO::FETCH_ASSOC)["asiakasnro"];
 
-    $query = $db->prepare("INSERT INTO tuoteryhma (ktunnus, asiakasnro, admincheck, salasana) VALUES (:ktunnus, :asiakasnro, 0, :salasana)");
+    if(empty($asiakasnro)) {
+        throw new Exception("Asiakasnumeroa ei löytynyt!");
+    }
+
+    $query = $db->prepare("INSERT INTO tunnus (ktunnus, asiakasnro, salasana) VALUES (:ktunnus, :asiakasnro, :salasana)");
     $query->bindValue(":ktunnus", $ktunnus, PDO::PARAM_STR);
     $query->bindValue(":asiakasnro", $asiakasnro, PDO::PARAM_STR);
     $query->bindValue(":salasana", $salasana, PDO::PARAM_STR);
@@ -30,7 +35,7 @@ try {
     $query->execute();
 
     header("HTTP/1.1 200 OK");
-    $data = array("id" => $db->lastInsertId(), "trnimi" => $trnimi);
+    $data = array("id" => $db->lastInsertId(), "asiakasnro" => $asiakasnro);
     print json_encode($data);
 } catch (PDOException $pdoex) {
     $db->rollback();

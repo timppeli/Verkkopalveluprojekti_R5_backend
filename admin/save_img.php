cfcd792702b47327f6dca72daf6a09ff14acf793
Aleksi Viitanen <n1vial00@students.oamk.fi>
2022-04-30 13:10:54 +0300

    Pieni muutos kuvan vaihtoa varten.

--- admin/save_img.php
+++ admin/save_img.php
@@ -10,6 +10,7 @@ $tuotenro = filter_input(INPUT_POST, "tuotenro");
 $file_tmp = $_FILES['file']['tmp_name'];
 $file_ext = strtolower(end(explode('.',$_FILES['file']['name'])));
 $file = $folderPath . "tuotenro_" . $tuotenro . '.'.$file_ext;
+unlink($file);
 move_uploaded_file($file_tmp, $file);
 
 print json_encode($tuotenro);
